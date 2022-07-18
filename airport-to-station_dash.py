e234e617638dfab7128fca16eccb8103062cbba3
Robert Duc Bui <robertducbui@gmail.com>
2022-07-18 01:15:41 -0500

    (WIP) removing redundancies - cleaning for migration to dash-leaflet over folium

--- airport-to-station_dash.py
+++ airport-to-station_dash.py
@@ -1,41 +1,31 @@
-!pip install airportsdata
-!pip install dash
-!pip install haversine
-
-import pandas as pd
-import airportsdata as ad
-
-import haversine
-from haversine import haversine, haversine_vector, Unit
-
 import dash
-import dash_html_components as html
-import plotly.graph_objects as go
 import dash_core_components as dcc
-from dash.dependencies import Input, Output
+import dash_html_components as html
 import folium
-import matplotlib.pyplot as plt
+import pandas as pd
+from dash.dependencies import Input, Output
 from folium import plugins
 
 app = dash.Dash()
-distances = pd.read_pickle('uge_deicing_heatwatch_2022/data_noaa/distances.pkl')
-df_stations = pd.read_pickle('uge_deicing_heatwatch_2022/data_noaa/df_stations.pkl')
-airports = pd.read_pickle('uge_deicing_heatwatch_2022/data_noaa/airports.pkl')
+distances = pd.read_pickle('data_noaa/distances.pkl')
+df_stations = pd.read_pickle('data_noaa/df_stations.pkl')
+airports = pd.read_pickle('data_noaa/airports.pkl')
 
-app.layout = html.Div(id = 'parent', children = [
-    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
-                                            'marginTop':40,'marginBottom':40}),
+app.layout = html.Div(id='parent', children=[
+    html.H1(id='H1', children='Styling using html components', style={'textAlign': 'center',
+                                                                      'marginTop': 40, 'marginBottom': 40}),
 
-        dcc.Input(id = 'textinput',
-            placeholder='Enter 3-letter IATA code',
-            type='text',
-            value='ORD'
-        ),
+    dcc.Input(id='textinput',
+              placeholder='Enter 3-letter IATA code',
+              type='text',
+              value='ORD'
+              ),
         dcc.Graph(id = 'foliummap')
     ])
 
-@app.callback(Output(component_id='foliummap', component_property= 'figure'),
-              [Input(component_id='textinput', component_property= 'value')])
+
+@app.callback(Output(component_id='foliummap', component_property='graph'),
+              [Input(component_id='textinput', component_property='value')])
 
 def draw_top5_map(input_iata):
   top5stations = distances[[input_iata]]\
@@ -70,17 +60,19 @@ def draw_top5_map(input_iata):
   for i in range(0,len(top5stations)):
     folium.Marker(
         [top5stations.iloc[i]['lat'],top5stations.iloc[i]['lon']],
-        popup = top5stations.iloc[i]['id'] + "\n" + top5stations.iloc[i]['name'] + "\n" + str(top5stations.iloc[i]['distance']),
-        icon = folium.Icon(color='green',
-                        icon='cloud',
-                        prefix='fa')
+        popup=top5stations.iloc[i]['id'] + "\n" + top5stations.iloc[i]['name'] + "\n" + str(
+            top5stations.iloc[i]['distance']),
+        icon=folium.Icon(color='green',
+                         icon='cloud',
+                         prefix='fa')
     ).add_to(m5)
 
   # Init + add minimap
   minimap = plugins.MiniMap()
   m5.add_child(minimap)
 
-  return(m5)
+  return m5
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     app.run_server()
